deed23f79f1966528c62b72e7daaa038d623ff5a
Tharushka Dinujaya <tharushkadinujaya05@gmail.com>
2026-06-20 01:07:11 +0530

    chore: update dependencies and add new packages for improved functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Bai_Jamjuree } from 'next/font/google'
+import { Geist, Geist_Mono, Bai_Jamjuree, Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,7 +34,15 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${bai.variable} h-full antialiased`}
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        bai.variable,
+        'font-sans',
+        inter.variable
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
