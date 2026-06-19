ac396b6247bc7e1eec84b765939ad065067bbf2d
Tharushka Dinujaya <tharushkadinujaya05@gmail.com>
2026-06-20 02:22:00 +0530

    feat: refactor login and signup forms to use card components for better UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,18 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google'
+import { Poppins, Geist_Mono } from 'next/font/google'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import './globals.css'
 import { cn } from '@/lib/utils'
 
-const geistSans = Geist({
+const poppins = Poppins({
   subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans'
 })
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin']
-})
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-playfair'
 })
 
 export const metadata: Metadata = {
@@ -35,9 +30,8 @@ export default function RootLayout({
       lang="en"
       className={cn(
         'h-full antialiased',
-        geistSans.variable,
+        poppins.variable,
         geistMono.variable,
-        playfair.variable,
         'font-sans'
       )}
     >
